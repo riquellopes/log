7402cf5993d7f8005ddc1ca30f91f7094de81a51
Henrique Lopes <riquellopes@gmail.com>
2017-04-29 22:57:40 -0300

    adding conf memory in env

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 # coding: utf-8
+import os
 
 GRAPH_FB = "http://graph.facebook.com/{}"
 DEBUG = True
 SECRET_KEY = 'ldeyrweuy11yu2333uy3iu'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///fb.db'
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
